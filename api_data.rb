48827a9f007ff68186bf19296f1b74d192c5c169
Max Mitrani <mjmitrani168@gmail.com>
2020-07-26 09:35:30 -0600

    add start to harbinger service

--- api_data.rb
+++ api_data.rb
@@ -8,6 +8,9 @@ class ApiData < Sinatra::Base
       "Hello"
   end
 
+  get "/harbinger/:id" #unsure if there will be a parameter here
+    conn = Faraday.new(url: 'https://api.nasa.gov', params: {api_key: ENV['nasa_api_key']})
+  end
 
 
 end
