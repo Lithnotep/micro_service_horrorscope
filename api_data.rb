ae2b44f4b24f510238209e2b450f0f6b99d1c533
Max Mitrani <mjmitrani168@gmail.com>
2020-07-27 14:18:17 -0600

    add gemfile rename

--- api_data.rb
+++ api_data.rb
@@ -11,7 +11,8 @@ class ApiData < Sinatra::Base
 
 
   get "/harbinger/:asteroid_id" do
-    binding.pry#unsure if there will be a parameter here
+    binding.pry
+    #unsure if there will be a parameter here
     content_type :json
     NeosSerializer.new.harbinger(NeosService.new.harbinger(params[:asteroid_id])).to_json
   end
